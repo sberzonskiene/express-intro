6d1687158e0fb108ae73d23e407768329861fd55
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-03 13:59:43 +0300

    validation

---
+++ src/api/public/postRegister.js
@@ -0,0 +1,32 @@
+import { connection } from "../../db.js";
+import { IsValid } from "../../lib/isValid.js";
+export async function postRegister(req, res) {
+    const [err, msg] = IsValid.fields(req.body, {
+        username: 'username',
+        email: 'email',
+        password: 'password',
+    });
+
+    if (err) {
+        return res.json({
+            status: 'error',
+            msg: msg,
+        });
+    }
+
+    const { username, email, password } = req.body;
+
+    try {
+        const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?);`;
+        const response = await connection.execute(sql, [username, email, password]);
+
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+
+    return res.json({
+        status: 'success',
+        msg: 'Sekminga registracija',
+    });
+}
---
+++ src/lib/randomString.js
@@ -0,0 +1,10 @@
+export function randomString(size=20) {
+    const abc = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let str = '';
+
+    for (let i = 0; i < size; i++) {
+        str += abc[Math.floor(Math.random() * abc.length)];
+    }
+
+    return str;
+}
