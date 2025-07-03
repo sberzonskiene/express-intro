6f98222be48058521f659a1a337c39eac77d2b68
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-03 11:54:22 +0300

    favicon

--- src/template/PageTemplate.js
+++ src/template/PageTemplate.js
@@ -12,6 +12,11 @@ export class PageTemplate {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Express intro</title>
+            <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+            <link rel="icon" type="image/svg+xml" href="/favicon//favicon.svg" />
+            <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="manifest" href="/site.webmanifest" />
             <link rel="stylesheet" href="/css/main.css">
             <link rel="stylesheet" href="/css/vendor/font-awesome.min.css"/>
         </head> `;
