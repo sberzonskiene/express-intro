e82c1e154a8be8e4ffd4a0ae4cb47cd42822c5b5
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-04 08:36:34 +0300

    update

--- src/ui/forms/registerForm.js
+++ src/ui/forms/registerForm.js
@@ -1,20 +1,21 @@
 export function registerForm (){
     return `<h1><i class="fa fa-registered"></i>Register</h1>
-            <form id="registerForm">
-                <div class="mb-4">
-                    <label for="username">Username</label>
-                    <input id="username" type="text" required>
-                </div>    
-                <div class="mb-4">    
+            <form class="form">
+                    <div class="form-row">
+                        <label>Username</label>
+                        <input id="username" type="text" value="" required>
+                    </div>   
+                <div class="form-row">    
                     <label for="email">Email</label>
                     <input id="email" type="email" required>
                 </div>    
-                <div class="mb-4">    
+                <div class="form-row">    
                     <label for="password">Password</label>
                     <input id="password" type="password" required>
                 </div>    
-                <div class="mb-4">    
+                <div class="form-row">    
                     <button type="submit">Register</button>
                 </div>    
             </form>`;
 }
+
