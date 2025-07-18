971074f2ce971266a99361dfdeeabf286a6c4f64
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-18 13:27:46 +0300

    cookie

--- src/api/public/postLogin.js
+++ src/api/public/postLogin.js
@@ -1,6 +1,7 @@
 import { connection } from "../../db.js"
 import { IsValid } from "../../lib/IsValid.js";
 import { hash } from "../../lib/hash.js";
+import { randomString } from '../../lib/randomString.js';
 
 export async function postLogin(req, res) {
     const [err, msg] = IsValid.fields(req.body, {
@@ -25,7 +26,7 @@ export async function postLogin(req, res) {
         if (response.length === 0 ) {
             return res.status(400).json({
                 status: 'error',
-                msg: 'Username/password yra neteisingas',
+                msg: 'Username ir password pora yra neteisinga',
             });
         }
 
@@ -50,13 +51,15 @@ export async function postLogin(req, res) {
     if (hashedPassword !== userObj.password_hash) {
         return res.status(400).json({
             status: 'error',
-            msg: 'Username/password yra neteisingas',
+            msg: 'Username ir password pora yra neteisinga',
         });
     }
 
-    /*try {
-        const sql = `INSERT INTO users (username, salt, password_hash) VALUES (?, ?, ?);`;
-        const [response] = await connection.execute(sql, [username, salt, passwordHash]);
+    const loginTokenString = randomString();
+
+     try {
+        const sql = `INSERT INTO login_tokens (user_id, token) VALUES (?, ?);`;
+        const [response] = await connection.execute(sql, [userObj.id, loginTokenString]);
 
         if (response.affectedRows !== 1) {
             return res.status(500).json({
@@ -65,22 +68,28 @@ export async function postLogin(req, res) {
             });
         }
     } catch (error) {
-        if (error.code === 'ER_DUP_ENTRY') {
+        console.log(error);
         return res.status(500).json({
-            status: 'error',
-            msg: 'Kartojasi irasas...',
-        });
-    }
-    console.log(error);
-    return res.status(500).json({
             status: 'error',
             msg: 'Serverio klaida',
         });
-    } */
+    } 
 
-    return res.status(201).json({
-        status: 'success',
-        msg: 'Registration successful',
-    });
-}
+    const cookieParams = [
+        `loginToken=${loginTokenString}`,
+        'domain=localhost',
+        'max-age=3600',
+        'HttpOnly',
+        'path=/',
+        'Secure',
+        'SameSite=Lax',
+    ];
+
+    return res
+        .set({ 'Set-Cookie': cookieParams.join('; ') })
+        .json({
+            status: 'succsess',
+            msg: 'Tu buvai sekmingai prijungtas prie sistemos',
+        });
+    }
  
